9a002a8fd4fef13ed8a00a0822437b77046b23d8
AI_Georgevici <adrian-iustin.georgevici@ruhr-uni-bochum.de>
2020-12-12 11:10:47 +0100

    new data cleaning pregnant data

--- import_data.R
+++ import_data.R
@@ -9,28 +9,89 @@
 library(here)
 library(readxl)
 library(tidyverse)
-library(janitor)
+library(skimr)
+
+# list of dropped variables:
+# "NW","Einleitung","Bemerkung","Indikation","Vorerkrankung","VE","auf Neo2"
+# "SaO2 , Austreibungsphase 2[min] , BZ2 , Temp2 , pH2 ven , Reifegrad2 , Rea2 ,
+# Apgar1min2 , Apgar5min2 , Apgar 10min2 , pH2 art , BE2 , Gew2 , Größe2 , Geschlecht2 ,
+# O2;2 , Maske2 , Intub2 , Volu2 , Pufferung2"
 
 
-library(DataExplorer)
+rowdata <- read_excel("input/rohdata.xls") %>% as_tibble() %>%
+  rename(ID = 1) %>% mutate(ID = as.factor(ID)) 
 
+vars_2ndchild <- enframe(map_dbl(rowdata, ~ mean(is.na(.x)))) %>%
+  arrange(desc(value)) %>% slice(1:20) %>% pull(name)
 
-check_struct <- function(df){
-  suppressWarnings(
-    df <- df %>% purrr::map_dfr(suppressWarnings(mosaic::favstats)) %>%
-      cbind(names = names(df)) %>% 
-      dplyr::select(names, everything()) %>%  
-      mutate(NAs = 100*missing/nrow(df)) %>% 
-      cbind(NA_NULL = map_dbl(df, ~ 100*sum(is.na(dplyr::na_if(.,0)))/nrow(df))) %>% 
-      dplyr::select(-c(n, missing)) %>% 
-      #mutate(imp = sd*(100-perc_NA)) %>% mutate(imp = imp/sum(imp)) %>% 
-      mutate_if(is.numeric, ~ round(., digits = 1)) 
+data <- rowdata %>% 
+  select(-any_of(c("NW","Einleitung","Bemerkung","Indikation","Vorerkrankung","VE","auf Neo2",vars_2ndchild))) %>% 
+  separate(SSW, into = c("week","day")) %>% 
+  mutate(
+    across(.cols = c(week,day), ~ as.integer(.x)),
+    across(.cols = c(week,day), ~ replace_na(.x, 0)),
+    pregnancy_days = week + day
+  ) %>% 
+  select(-c(week,day)) 
+
+rm(vars_2ndchild)
+
+data <- data %>% 
+  mutate(
+    Hb = round(as.double(ifelse(Hb == "12,2,","12.2",Hb)),1),
+    stillen =  replace_na(as.integer(na_if(stillen, "?")),1)
+  ) %>% 
+  rename(Reifegrad = `Reifeegrad1 (0=reif/1=unreif/2=überreif`) %>% 
+  mutate_at(vars(Reifegrad),
+            ~ case_when(. == "1" ~ -1,
+                        . == "unreif" ~ -1,
+                        . == "überreif" ~ 1,
+                        . == "2" ~ 1,
+                        TRUE ~ 0
+            ) %>% as.numeric()
+  ) %>% 
+  mutate_at(vars(Narkoseform),
+          ~ case_when(
+            is.na(.) ~ "spontan",
+            . == "Spa" ~ "SPA",
+            . == "SPA" ~ "SPA",
+            . == "PDA" ~ "PDA",
+            . == "ITN" ~ "ITN",
+            . == "PDA/SPA" ~ "SPA",
+            TRUE ~ "combined")
+  ) %>% 
+  mutate(
+    Blutverlust = na_if(as.integer(parse_number(Blutverlust)),7),
+    Trapanal = ifelse(is.na(Trapanal),0,1)
   )
-  return(df)
-}
+    
 
-rowdata <- read_excel("input/rohdata.xls") %>% as_tibble() %>% clean_names() %>% 
-  select(-temp2, - reifegrad2, -nw, -vorerkrankung, -trapanal, -ve, -indikation, -bemerkung, -einleitung) %>% 
+missing_values <- list(
+rename(enframe(map_dbl(filter(data,Narkoseform=="spontan"),~mean(is.na(.x)))),spontan=value),
+rename(enframe(map_dbl(filter(data,Narkoseform=="ITN"),~mean(is.na(.x)))),itn=value),
+rename(enframe(map_dbl(filter(data,Narkoseform=="combined"),~mean(is.na(.x)))),combined=value),
+rename(enframe(map_dbl(filter(data,Narkoseform=="SPA"),~mean(is.na(.x)))),spa=value),
+rename(enframe(map_dbl(filter(data,Narkoseform=="PDA"),~mean(is.na(.x)))),pda=value)
+) %>% 
+reduce(full_join, by = "name") %>% 
+mutate(pmax = pmax(spontan,itn,combined,spa,pda), pmin = pmin(spontan,itn,combined,spa,pda)) %>% 
+filter(pmax > 0) %>% 
+mutate(across(.cols = spontan:last_col(), ~ round(.x, digits = 2))) %>% 
+arrange(desc(pmin)) 
+
+data <- select(data, -any_of(filter(missing_values,pmin>0.5)$name))
+
+missing_values %>% select(-c(pmax:pmin)) %>% 
+  gt(rowname_col = "name") %>% 
+  fmt_percent(columns = 2:6, decimals = 0)
+
+
+
+
+
+
+
+select(-temp2, - reifegrad2, -nw, -vorerkrankung, -trapanal, -ve, -indikation, -bemerkung, -einleitung) %>% 
   mutate(stillen = as.integer(stillen), blutverlust = as.integer(blutverlust)) %>% 
   rename(reifegrad = reifeegrad1_0_reif_1_unreif_2_uberreif) %>% 
   replace_na(list(einleitung = "keine", narkoseform = "keine", reifegrad = "0")) %>% 
